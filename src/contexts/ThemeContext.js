f5256e50f4cb2c7266b753faa57de16b83dca689
vandana104 <vandanaoel@gmail.com>
2024-06-10 11:48:10 +0530

    Completed

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,21 +1,3 @@
-// import { createContext, useState } from "react";
-
-// export const ThemeContext = createContext("light");
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState("light");
-
-//   const toggleTheme = () => {
-//     setTheme((current) => (current === "light" ? "dark" : "light"));
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-
 import { createContext, useState } from "react";
 
 export const ThemeContext = createContext("light");
